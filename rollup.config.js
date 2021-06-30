cd865f309b9ba531564502edb2c2748176166ff2
Andreas Rozek <A.Rozek@gmx.de>
2021-06-30 17:23:31 +0200

    update build configuration

--- rollup.config.js
+++ rollup.config.js
@@ -6,19 +6,11 @@ import resolve        from '@rollup/plugin-node-resolve'
 import autoPreprocess from 'svelte-preprocess'
 import typescript     from '@rollup/plugin-typescript'
 import postcss        from 'rollup-plugin-postcss'
-import { terser }     from 'rollup-plugin-terser'
 
 export default {
   input: './src/index.ts',
   output: [
     {
-      file:     './dist/svelte-sortable-flat-list-view.js',
-      format:    'umd', // builds for both Node.js and Browser
-      name:      'sortableFlatListView', // required for UMD modules
-      noConflict:true,
-      sourcemap: true,
-      plugins: [terser({ format:{ comments:false, safari10:true } })],
-    },{
       file:     './dist/svelte-sortable-flat-list-view.esm.js',
       format:   'esm',
       sourcemap:true,
